d5bb3165ce3afeb420e2377b189f6b5fb70511ec
Lucia Izquierdo Delgado <lucygm.dev@gmail.com>
2021-09-10 14:01:08 +0200

    Making Responsive

--- js/index.js
+++ js/index.js
@@ -2,15 +2,24 @@
 
 const createErrorMessage = (blockInput, message) => {
   const error_message = document.createElement("span");
-  error_message.className += " error-message";
+  error_message.className += "error-message";
   error_message.innerHTML = message;
 
   blockInput.appendChild(error_message);
 };
 
+const deleteErrorMessages = () => {
+  const error_messages = document.querySelectorAll(".error-message");
+  error_messages.forEach((message) => {
+    const parentNode = message.parentNode;
+    parentNode.removeChild(message);
+  });
+};
+
 const emailForm = document.querySelector("#email-form");
 emailForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
+  deleteErrorMessages();
   const emailInput = document.querySelector("#email-input");
   const email = emailInput.value;
   if (email === "") {
